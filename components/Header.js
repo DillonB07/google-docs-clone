e1d1cb9c8204c2cb75489220001c3fd4b7d76a95
DillonB07 <dillonbarnes07@gmail.com>
2022-03-14 18:50:20 +0000

    Material-Tailwind bug introduced here

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Button from '@material-tailwind/react/Button'
+
+function Header() {
+  return (
+    <header className="flex items-center">
+      <Button color="gray" buttonType="outline"></Button>
+    </header>
+  )
+}
+export default Header
