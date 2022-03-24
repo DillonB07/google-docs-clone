f4a93fcc6a0c74730b943365e6b122a7065fa683
DillonB07 <dillonbarnes07@gmail.com>
2022-03-24 17:30:14 +0000

    Add auto-refresh functionality when creating document

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,14 @@ import ModalFooter from '@material-tailwind/react/ModalFooter'
 import Button from '@material-tailwind/react/Button'
 import Icon from '@material-tailwind/react/Icon'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { db } from '../firebase'
 import firebase from 'firebase'
 import { useCollectionOnce } from 'react-firebase-hooks/firestore'
 
 export default function Home() {
   const [session] = useSession()
+  const router = useRouter()
 
   if (!session) return <Login />
 
@@ -38,6 +40,7 @@ export default function Home() {
     })
     setInput('')
     setShowModal(false)
+    router.reload()
   }
 
   const modal = (
